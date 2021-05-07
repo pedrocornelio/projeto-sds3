6b88000348158e641f5a58ae4797f3b229c9cd13
Pedro Cornelio <pedrocornelio@gmail.com>
2021-05-07 13:45:19 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,9 @@
-import React from 'react';
-import NavBar from "components/NavBar";
-import Footer from 'components/Footer';
-import DataTable from 'components/DataTable';
-import BarChart from 'components/BarChart';
-import DonutChart from 'components/DonutChart';
+import Routes from 'Routes';
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">DASHBOARD</h1>
-
-        <div className="row px-3">
-          <div className="col sm-6">
-          <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
-            <BarChart />
-          </div>
-          <div className="col sm-6">
-            <h5 className="text-center text-secondary">Todas as Vendas</h5>
-            <DonutChart />
-          </div>
-        </div>
-
-        <div>
-          <h2 className="text-primary">Todas Vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
-
 
 export default App;
---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,19 @@
+import Home from "pages/Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/dashborad">
+                    <Home />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
